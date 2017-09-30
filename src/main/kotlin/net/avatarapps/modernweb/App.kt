544d8b1bd41e379399ef05756fa4f19ac1fb6852
islam <islam.a.elamin@gmail.com>
2017-09-30 22:53:07 +0300

    Working on language

--- src/main/kotlin/net/avatarapps/modernweb/App.kt
+++ src/main/kotlin/net/avatarapps/modernweb/App.kt
@@ -9,40 +9,47 @@ import net.avatarapps.modernweb.core.drawable.Color
 
 fun main(args: Array<String>) {
 
+    val redView = View().apply {
+        width = 300.pixels
+        height = 500.pixels
+        background = Color(red = 255, green = 0, blue = 0)
+    }
+
+    val blueView = View().apply {
+        width = 300.pixels
+        height = 100.pixels
+        background = Color(red = 0, green = 0, blue = 255)
+        marginTop = 20.pixels
+    }
+
+    val greenView = View().apply {
+        width = 600.pixels
+        height = 200.pixels
+        background = Color(red = 0, green = 255, blue = 0)
+        marginStart = 20.pixels
+    }
+
+    val leftLayout = LinearLayout(orientation = vertical).apply {
+        background = Color(220,220,220)
+        width = 300.pixels
+        height = 400.pixels
+        setPadding(20.pixels)
+        isScrollableVertically = true
+        add(redView)
+        add(blueView)
+    }
+
+    val bigLayout = LinearLayout(orientation = horizontal).apply {
+        background = Color(200,200,200)
+        width = 1500.pixels
+        height = 600.pixels
+        setPadding(20.pixels)
+        add(leftLayout)
+        add(greenView)
+    }
+
     page {
-        root = LinearLayout(orientation = horizontal).apply {
-            background = Color(200, 200, 200)
-            width = 1500.pixels
-            height = 600.pixels
-            setPadding(20.pixels)
-            add(LinearLayout(orientation = vertical).apply {
-                background = Color(220, 220, 220)
-                width = 300.pixels
-                height = 400.pixels
-                setPadding(20.pixels)
-                isScrollableVertically = true
-                add(View().apply {
-                    width = 300.pixels
-                    height = 500.pixels
-                    background = Color(red = 255, green = 0, blue = 0)
-                })
-
-                add(View().apply {
-                    width = 300.pixels
-                    height = 100.pixels
-                    background = Color(red = 0, green = 0, blue = 255)
-                    marginTop = 20.pixels
-                })
-            })
-
-            add(View().apply {
-                width = 600.pixels
-                height = 200.pixels
-                background = Color(red = 0, green = 255, blue = 0)
-                marginStart = 20.pixels
-            })
-        }
-
+        root = bigLayout
     }
 }
 
