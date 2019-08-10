89796fad7c9ddb5ed2ab0fd21647c0ffbf75fe18
Islam Abdalla <islam.a.elamin@gmail.com>
2019-08-10 12:02:10 +0200

    add view to parent before configuring view in visit

--- src/main/kotlin/com/narbase/kunafa/core/components/visit.kt
+++ src/main/kotlin/com/narbase/kunafa/core/components/visit.kt
@@ -4,10 +4,10 @@ import com.narbase.kunafa.core.lifecycle.LifecycleObserver
 
 fun <V : View> V.visit(lifecycleObserver: LifecycleObserver?, setup: V.() -> Unit): V {
     lifecycleObserver?.let { bind(it) }
+    this.addToParent()
     configureElement()
     this.setup()
     postOnViewCreated()
-    this.addToParent()
     return this
 }
 
