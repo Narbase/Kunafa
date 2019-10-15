6301674b2e9042c144a40676a44001c4fbdbf047
Islam Abdalla <islam.a.elamin@gmail.com>
2019-10-15 09:19:03 +0200

    fix bug where at rule not taken into account when calculating ruleset hashcode

--- src/main/kotlin/com/narbase/kunafa/core/css/RuleSet.kt
+++ src/main/kotlin/com/narbase/kunafa/core/css/RuleSet.kt
@@ -13,8 +13,8 @@ class RuleSet(var selector: Selector, val atRule: String? = null) {
 
     override fun hashCode(): Int {
         return rules.sumBy { it.hashCode() } +
-                ((subRuleSets?.sumBy { it.hashCode() * it.selector.toString().hashCode() }) ?: 0) +
-                (atRuleSets?.sumBy { it.hashCode() * it.selector.toString().hashCode() } ?: 0)
+                ((subRuleSets?.sumBy { it.hashCode() * "${it.selector}".hashCode() }) ?: 0) +
+                (atRuleSets?.sumBy { it.hashCode() * "${it.selector}${it.atRule}".hashCode() } ?: 0)
     }
 
 
