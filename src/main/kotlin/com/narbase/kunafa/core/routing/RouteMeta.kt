ee954202ab3aed14200880e3776bb33f67425fe9
Islam Abdalla <islam.a.elamin@gmail.com>
2020-03-10 21:22:21 +0200

    documentation for RouteMeta

--- src/main/kotlin/com/narbase/kunafa/core/routing/RouteMeta.kt
+++ src/main/kotlin/com/narbase/kunafa/core/routing/RouteMeta.kt
@@ -6,12 +6,15 @@ import com.narbase.kunafa.core.lifecycle.Observable
  * Copyright 2017-2020 Narbase technologies and contributors. Use of this source code is governed by the MIT License.
  */
 
-/*
- * Copyright 2017-2020 Narbase technologies and contributors. Use of this source code is governed by the MIT License.
+/**
+ * @param path: the route path
+ * @param params the route parameters used in :<param>
  */
 class RouteMeta(val path: String, val params: Observable<Map<String, String>>) {
-    /*
- * Copyright 2017-2020 Narbase technologies and contributors. Use of this source code is governed by the MIT License.
- */
+    /**
+     * Called before the route changes. If the return value is false, then route will not
+     * change
+     * @return true to allow navigating, and false to prevent navigation away
+     */
     var onRouteWillChange: (() -> Boolean)? = null
 }
